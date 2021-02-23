6c75ef718a30f86c05fe2de7788ca4b18de91052
dahiyai <dahiyai@sheridancollege.ca>
2021-02-23 11:24:42 -0500

    added usernamevalidator

---
+++ src/midtermreviewcodeforpartc/UserNameValidator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermreviewcodeforpartc;
+
+/**
+ *
+ * @author ishan
+ */
+public class UserNameValidator {
+    private String Username;
+    
+    public UserNameValidator(String empty){
+        String n = null;
+        if(n != " "){
+            System.out.println("empty userName");
+        }
+    };
+    
+    
+}
